ce996806c17fefcb7228ff8edb4f9d7a0a4b20e2
te <mynameistecs51@gmail.com>
2014-03-09 23:32:48 +0700

    te update 9-3-57 23.33

--- application/views/page_teacher.php
+++ application/views/page_teacher.php
@@ -1,21 +1,23 @@
 <?php $this->load->view("head");?>
 <div class="row">
-  <div class="col-md-4">
-      <img src="<?php echo base_url().'image/pict_teacher/a.jpg';?>"  width="140px" height="200px"/>
-  </div>
-  <div class="col-md-4 col-md-offset-4">
-      <?php 
-      foreach ($teacher as $row)  {
-        echo $row->teacher_name;
-    }
-    ?>
-</div>
-</div>
-<div class="row">
-  <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
-  <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
+    <div class="col-md-3 col-md-offset-3">
+        <div class="panel panel-primary" align="center">
+            <?php 
+            // foreach ($teacher as $row) {
+            //     echo anchor('www.google.com','<div class="panel-body" >
+            //         <img src=" '.base_url().'image/pict_teacher/'.$row->teacher_name.'.jpg"  style="width:200px; hight:150px; padding:3px;">    
+            //     </div>
+            //     <div class="panel-footer" align="center">'.$row->teacher_name.'</div>  ');
+            // }?>    
+            <a href="www.google.com">  <img src="<?php echo base_url().'image/pict_teacher/ab.jpg' ?>">  </a>  
+        </div>
+    </div>
 </div>
 <div class="row">
-  <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
+    <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
+    <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
+</div>
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
 </div>
 <?php $this->load->view("footer");?>
