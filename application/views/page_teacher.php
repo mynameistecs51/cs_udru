bebd0aedf071af084d82245835cbf055451f3fd6
te <mynameistecs51@gmail.com>
2014-03-12 00:11:13 +0700

    te update 12-3-57 0.11

--- application/views/page_teacher.php
+++ application/views/page_teacher.php
@@ -7,9 +7,9 @@
       foreach ($teacher as $key=> $value) { 
         if($value->teacher_number == '1'){
           echo anchor($value->teacher_link,'<div class="panel-body" >
-            <img src=" '.base_url().'image/pict_teacher/'.$value->teacher_name.'.jpg"  class="img-rounded" style="width:100px; hight:70px; padding:1px;">    
+            <img src=" '.base_url().'image/pict_teacher/'.$value->teacher_pict.'.jpg"  class="img-rounded" style="width:200px; hight:140px;">    
           </div>
-          <div class="panel-footer" align="center">'.$value->teacher_preName.' '.decoct($value->teacher_name).'<br/>ประธานสาขาวิชา'.'</div>');
+          <div class="panel-footer" align="center">'.$value->teacher_preName.' '.$value->teacher_name.'<br/>ประธานสาขาวิชา'.'</div>');
         }
         ?>    
 
@@ -22,7 +22,7 @@
    echo ' <div class="col-md-3 col-md-offset-0">';
    echo '<div class="panel panel-primary" align="center">';     
    echo anchor($value->teacher_link,'<div class="panel-body" >
-    <img src=" '.base_url().'image/pict_teacher/'.$value->teacher_name.'.jpg"  class="img-rounded" style="width:100px; hight:70px; padding:1px;">    
+    <img src=" '.base_url().'image/pict_teacher/'.$value->teacher_name.'.jpg"  class="img-rounded" style="width:200px; hight:140px;">    
   </div>
   <div class="panel-footer" align="center">'.$value->teacher_preName.' '.decoct($value->teacher_name).'</div> ');     
    echo '</div>'; 
