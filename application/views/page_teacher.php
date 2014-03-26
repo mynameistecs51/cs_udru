043209574f84e9dbca79926daeca8daceaec59e1
chaiwat <mynameistecs51@gmail.com>
2014-03-26 10:08:34 +0700

    chaiwat update 26-03-57 10.08

--- application/views/page_teacher.php
+++ application/views/page_teacher.php
@@ -1,33 +1,33 @@
 <?php $this->load->view("head");?>
 <div class="row">
-  <div class="col-md-3 col-md-offset-4">
-    <div class="panel panel-primary" align="center">
+ <div class="col-md-3 col-md-offset-4">
+  <div class="panel panel-primary" align="center">
 
-      <?php 
-      foreach ($teacher as $key=> $value) { 
-        if($value->teacher_number == '1'){
-          echo anchor($value->teacher_link,'<div class="panel-body" >
-            <img src=" '.base_url().'image/pict_teacher/'.$value->teacher_pict.'.jpg"  class="img-rounded" style="width:180px; hight:90px;">    
-          </div>
-          <div class="panel-footer" align="center">'.$value->teacher_preName.' '.$value->teacher_name.'<br/>ประธานสาขาวิชา'.'</div>');
-        }
-        ?>    
+   <?php 
+   foreach ($teacher as $key=> $value) { 
+    if($value->teacher_number == '1'){
+     echo anchor($value->teacher_link,'<div class="panel-body" >
+      <img src=" '.base_url().'image/pict_teacher/'.$value->teacher_pict.'.jpg"  class="img-rounded" style="width:180px; hight:90px;">    
+   </div>
+   <div class="panel-footer" align="center">'.$value->teacher_preName.' '.$value->teacher_name.'<br/>ประธานสาขาวิชา'.'</div>');
+  }
+  ?>    
 
-      </div>
-    </div>
-  </div>
-  <!-- <div class="row"> -->
-  <?php 
-  if($value->teacher_number > '1'){   
-   echo ' <div class="col-md-4 col-md-offset-0">';
+</div>
+</div>
+</div>
+<!-- <div class="row"> -->
+<?php 
+if($value->teacher_number > '1'){   
+   echo ' <div class="col-md-3 col-md-offset-0">';
    echo '<div class="panel panel-primary" align="center">';     
    echo anchor($value->teacher_link,'<div class="panel-body" >
-    <img src=" '.base_url().'image/pict_teacher/'.$value->teacher_pict.'.jpg"  class="img-rounded" style="width:180px; hight:90px;">      
+     <img src=" '.base_url().'image/pict_teacher/'.$value->teacher_pict.'.jpg"  class="img-rounded" style="width:180px; hight:90px;">      
   </div>
   <div class="panel-footer" align="center">'.$value->teacher_preName.' '.$value->teacher_name.'</div> ');     
    echo '</div>'; 
    echo '</div>';
- }
+}
 };?>   
 <!--  ////---------------end foreacher--------------- //// -->
 
