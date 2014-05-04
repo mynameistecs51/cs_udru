bed2a3e5c75ffd26acfd75e2bd581c67ec38a992
te <te.chaiwat@gmail.com>
2014-05-04 19:07:18 +0700

    te update 4-5-57 19.07

--- application/views/cs_udru.php
+++ application/views/cs_udru.php
@@ -18,42 +18,21 @@
 		</div>
 	</div>
 </div>
+<!-- //////////////  post new ///////////// -->
 <div class="row col-md-8" style="padding-left:3px;">
-	<div class="panel panel-primary " >
-
-		<?php 
-		foreach ($teacher_news as $key_news => $value_news) {
-			echo  '<div class="panel-heading"><b class="glyphicon glyphicon-info-sign"></b>  '.$value_news ->news_head.'</div>';
-			echo '<div class="panel-body alert-info" style="padding:10px; word-wrap:break-word;" >';
-			echo $value_news ->news_container;
-			echo '<br/><span class="label label-warning" style="float:right;">'.$value_news->news_peoplePost.'</span> ';
-			//echo "<br/>";
-			echo '<br/><span class="label label-warning" style="float:right;">โพสเมื่อ '.$value_news->news_datePost.'</span> ';
-			echo "</div>";
-		}
-		?>		
-	</div>
-	<!-- //////// infomation /// -->
-	<!-- <div class="panel panel-primary" style="paddint-top:10px;" >
-		<div class="panel-heading"><b class="glyphicon glyphicon-info-sign"></b> 
-			ข่าวสาร
-		</div>
-		<div class="panel-body alert-info" style="padding:10px; word-wrap:break-word;" >
-			11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-			<br/><span class="label label-warning" style="float:right;">ผู้โพส</span> 
-		</div>				
-	</div>
-
-	<div class="panel panel-primary" style="paddint-top:10px;" >
-		<div class="panel-heading"><b class="glyphicon glyphicon-info-sign"></b> 
-			ข่าวสาร
-		</div>
-		<div class="panel-body alert-info" style="padding:10px; word-wrap:break-word;" >
-			222
-			<br/><span class="label label-warning" style="float:right;">ผู้โพส</span> 
-		</div>				
-	</div>
--->
-<!-- /// end infomation /// -->
+	
+	
+	<?php 
+	foreach ($teacher_news as $key_news => $value_news) {
+		echo "<div class='panel panel-primary ' >";		
+		echo  '<div class="panel-heading"><b class="glyphicon glyphicon-info-sign"></b>  '.$value_news ->news_head.'</div>';
+		echo '<div class="panel-body alert-info" style="padding:10px; word-wrap:break-word;" >';
+		echo $value_news ->news_container;
+		echo '<br/><span class="label label-warning" style="float:right;">'.$value_news->news_peoplePost.'</span> ';
+		echo '<br/><span class="label label-warning" style="float:right;">โพสเมื่อ '.$value_news->news_datePost.'</span> ';
+		echo "</div>";	echo "</div>";	
+	}
+	?>	
 </div>
+
 <?php $this->load->view("footer");?>
