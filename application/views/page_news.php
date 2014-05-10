0550f4c17c302215145f11a99a28fecc3c523199
te <te.chaiwat@gmail.com>
2014-05-10 23:49:13 +0700

    te update 10-5-57 23.49

--- application/views/page_news.php
+++ application/views/page_news.php
@@ -1,7 +1,7 @@
-﻿<?php $this->load->view("head");?>
+<?php $this->load->view("head");?>
 <div class="row  col-md-4" style="margin-right:40px;">
 	<div class="panel panel-primary " >
-		<div class="panel-heading"><b class="glyphicon glyphicon-user"></b>  ประธานสาขาวิชา</div>
+		<div class="panel-heading"><b class="glyphicon glyphicon-user"></b>  ผู้โพส</div>
 		<div class="panel-body alert-info" style="padding:10px;" align="center">
 			<div class="panel panel-primary " align="center">
 				<?php 
@@ -10,7 +10,7 @@
 						echo anchor($value->teacher_link,'<div class="panel-body" >
 							<img src=" '.base_url().'image/pict_teacher/'.$value->teacher_pict.'.jpg"  class="img-rounded" style="width:180px; hight:140px;">     
 						</div>
-						<div class="panel-footer" align="center">'.$value->teacher_preName.' '.$value->teacher_name.'<br/>ประธานสาขาวิชา'.'</div>');
+						<div class="panel-footer" align="center">'.$value->teacher_preName.' '.$value->teacher_name.'<br/>ผู้โพส'.'</div>');
 					}
 				}
 				?>  
@@ -26,7 +26,7 @@
 			echo "<div class='panel panel-primary ' >";		
 			echo  '<div class="panel-heading"><b class="glyphicon glyphicon-info-sign"></b>  '.$value_news ->news_head.'</div>'; ///////// ห้วข้อข่าว ///////////
 			echo '<div class="panel-body alert-info" style="padding:10px; word-wrap:break-word;" >';		
-			echo substr($value_news ->news_container,0,20)."...<span class='label label-info'>".anchor('ctl_main/index','อ่านต่อ')."</span>";  ///// เนื้อหาข่าว  ///////////
+			echo substr($value_news ->news_container,0,20)."...<span class='label label-warning'>อ่านต่อ</span>";  ///// เนื้อหาข่าว  ///////////
 			echo '<br/><span class="label label-warning" style="float:right;">'.$value_news->news_peoplePost.'</span> ';  //////คนโพสข่าว ////////// 
 			echo '<br/><span class="label label-warning" style="float:right;">โพสเมื่อ '.$value_news->news_datePost.'</span> '; ////////  วัน เวลา ที่ โพส /////////
 			echo "</div>";	echo "</div>";	
--- database_csudru/db_csudru.sql
+++ database_csudru/db_csudru.sql
@@ -36,14 +36,17 @@ CREATE TABLE IF NOT EXISTS `teacher` (
   `teacher_link` char(255) NOT NULL,
   `teacher_pict` char(100) NOT NULL,
   PRIMARY KEY (`teacher_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=47 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=50 ;
 
 --
 -- dump ตาราง `teacher`
 --
 
 INSERT INTO `teacher` (`teacher_id`, `teacher_user`, `teacher_pwd`, `teacher_preName`, `teacher_name`, `teacher_number`, `teacher_link`, `teacher_pict`) VALUES
-(46, 'aa', 'aa', 'aa', 'aa', 1, 'http://localhost/cs_udru/index.php/ctl_main/add_teacher', '2014-05-048774');
+(46, 'aa', 'aa', 'aa', 'aa', 1, 'http://localhost/cs_udru/index.php/ctl_main/add_teacher', '2014-05-048774'),
+(47, 'aa', 'aa', 'a', 'a', 2, '', '2014-05-047840'),
+(48, 'ฟห', 'ฟห', 'ฟห', 'ฟฟฟ', 3, '', '2014-05-041169'),
+(49, 'dd', 'dd', 'dd', 'dd', 3, '', '2014-05-041538');
 
 -- --------------------------------------------------------
 
@@ -58,14 +61,16 @@ CREATE TABLE IF NOT EXISTS `teacher_news` (
   `news_datePost` datetime NOT NULL,
   `news_peoplePost` varchar(50) NOT NULL,
   PRIMARY KEY (`news_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=33 ;
 
 --
 -- dump ตาราง `teacher_news`
 --
 
 INSERT INTO `teacher_news` (`news_id`, `news_head`, `news_container`, `news_datePost`, `news_peoplePost`) VALUES
-(30, '555', '555', '2014-05-04 15:01:26', 'chaiwat');
+(30, '555', '555', '2014-05-04 15:01:26', 'chaiwat'),
+(31, '2222', '22222', '2014-05-04 18:29:33', 'chaiwat'),
+(32, 'test', 'ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt', '2014-05-04 18:43:22', 'chaiwat');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
