90b3f37c1754b6fce04a143eed8b5647f8e4f968
te <te.chaiwat@gmail.com>
2014-05-13 19:45:12 +0700

    te update function check login

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,17 +4,21 @@ session_start();
 class Home extends CI_Controller{
 	function __construct(){
 		parent::__construct();
+		$this->load->model('model_main');
 	}
 
 	function index(){		
 		if($this->session->userdata('logged_in')){
-			$session_data = $this->session->userdata('logged_in');
-				//$data['username'] = $session_data['username']; // เมื่อสำเร็จแล้วให้เปลี่ยน title เป็น username
-				$data = array(
-					'username' => $session_data['username'],
-					'password' => $session_data['password'],
-					);
-				$this->load->view('home_view',$data);
+			// $session_data = $this->session->userdata('logged_in');
+			// 	$data['username'] = $session_data['username']; // เมื่อสำเร็จแล้วให้เปลี่ยน title เป็น username
+			// 	$data = array(					
+			// 		'title' => "CS_UDRU",
+			// 		'teacher' => $this->model_main->get_teacher(),
+			// 		'teacher_news' => $this->model_main->get_news(),
+			// 		'username' => $session_data['username'],
+			// 		);
+			// 	$this->load->view('cs_udru',$data);
+			redirect('ctl_main','refresh');
 			}else{
 				// if no session redirect to login page
 				redirect('ctl_main/login','refresh');
