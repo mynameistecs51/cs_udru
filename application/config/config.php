e0825620f43833dd949396e4c176ef25418b7275
tete <mynameistecs51@gmail.com>
2014-06-01 18:35:38 +0700

    tete update file config-->base url; database-->user&pass&db_name

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://te.dn-soft.net/';
+$config['base_url']	= 'http://localhost/cs_udru/';
 
 /*
 |--------------------------------------------------------------------------
